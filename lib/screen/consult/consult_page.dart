605d9e2581c47a6eeb4d6b53e85a4cc7415c5921
Roger Araujo <theroogers@gmail.com>
2022-06-10 20:59:14 -0300

    create a closed chamado

--- lib/screen/consult/consult_page.dart
+++ lib/screen/consult/consult_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../../shared/themes/app_colors.dart';
+import '../../shared/themes/app_texts.dart';
 
 class ConsultPage extends StatefulWidget {
   const ConsultPage({Key? key}) : super(key: key);
@@ -51,11 +52,32 @@ class _ConsultPageState extends State<ConsultPage> {
                   return ListView.builder(
                     itemCount: data.size,
                     itemBuilder: (context, index) {
-                      return Text(
-                        'Chamado do tipo: ${data.docs[index]['tipo']}\n para a categoria:${data.docs[index]['categoria']}\n com criticidade ${data.docs[index]['criticidade']}\n Observação: ${data.docs[index]['observacao']}\n Data de Abertura: ${data.docs[index]['createdAt']}\n',
-                        style: const TextStyle(
-                          fontSize: 15,
-                        ),
+                      return Row(
+                        children: [
+                          Text(
+                            'Chamado do tipo: ${data.docs[index]['tipo']}\n para a categoria:${data.docs[index]['categoria']}\n com criticidade ${data.docs[index]['criticidade']}\n Observação: ${data.docs[index]['observacao']}\n Data de Abertura: ${data.docs[index]['createdAt']}\n',
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          ElevatedButton(
+                            child: const Text('Fechar'),
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                //tamanho do botao
+                                horizontal: 10,
+                                vertical: 10,
+                              ),
+                              primary: AppColors.laranja,
+                              elevation: 15,
+                              onPrimary: Colors.white,
+                              textStyle: StyleText.bottomLogin,
+                            ),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/consultclosed');
+                            },
+                          )
+                        ],
                       );
                     },
                   );
